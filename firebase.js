fd32a80ded73e4ae4171f0b37a07334f9d9fb499
futuresmartinvest-pixel <futuresmartinvest@gmail.com>
2025-12-12 01:16:21 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,27 @@
-// -------------------------
-// Firebase v10 CDN imports
-// -------------------------
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+// ------------------------------------------------------
+// Firebase v10 CDN Imports
+// ------------------------------------------------------
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-// -------------------------
-// Your Firebase Config
-// -------------------------
+// ------------------------------------------------------
+// Your Firebase Config (PrimeInvestProject2)
+// ------------------------------------------------------
 const firebaseConfig = {
-  apiKey: "AIzaSyA1HZLZRY9UADNnDVBnB5nJ3mZyqqP4iY",
+  apiKey: "AIzaSyA1HZLZRY9UADNnDVBnoBIlG3b-bCkojxs",
   authDomain: "primeinvestproject2.firebaseapp.com",
   projectId: "primeinvestproject2",
-  storageBucket: "primeinvestproject2.appspot.com",
+  storageBucket: "primeinvestproject2.firebasestorage.app",
   messagingSenderId: "798977477628",
-  appId: "1:798977477628:web:c97d1fbd6da6e5b585c5f1"
+  appId: "1:798977477628:web:c97d1fba72ad7865864079"
 };
 
-// -------------------------
+// ------------------------------------------------------
 // Initialize Firebase
-// -------------------------
+// ------------------------------------------------------
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
